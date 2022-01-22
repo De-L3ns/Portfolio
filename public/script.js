6161dfcb68df8b08cfa93dac0c7d21811d3182ad
Laurens <70441547+De-L3ns@users.noreply.github.com>
2022-01-22 18:03:53 +0100

    Created base template for website

---
+++ public/script.js
@@ -0,0 +1,24 @@
+// Script for webpage dynamic
+
+
+// #Navbar sticky on scroll
+
+window.onscroll = function() {makeSticky()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function makeSticky() {
+
+    if (window.pageYOffset >= sticky) {
+
+        navbar.classList.add("sticky");
+
+    } else {
+
+        navbar.classList.remove("sticky");
+    }
+
+
+}
+
