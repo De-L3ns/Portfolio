f67bf84106cdb7ea1666337ae1023f40e10a8f80
Laurens <70441547+De-L3ns@users.noreply.github.com>
2022-01-25 22:45:47 +0100

    added JS to dynamicly add projects

--- public/script.js
+++ public/script.js
@@ -29,3 +29,47 @@ let information = ["Remake of a classic video game with Pygame", "Displays scrap
 let codeLinks = ["https://github.com/De-L3ns/Sea-Invaders", "https://github.com/De-L3ns/MusicScraper", "https://github.com/De-L3ns/Calculator"];
 let liveLinks = ["https://youtu.be/MM0fKFH9UYA","https://music-scraper-node.herokuapp.com", "https://de-l3ns.github.io/Calculator/"];
 
+const project = document.getElementById("projects");
+
+
+for (let i = 0; i < projects.length; i++) {
+
+    // divs for project info
+    let projectInfo = document.createElement("div");
+    projectInfo.classList.add("project-info");
+    project.appendChild(projectInfo);
+    
+    // text inside project info
+    let title = document.createElement("h2");
+    let projectContent = document.createElement("p");
+    title.textContent = projects[i];
+    projectContent.textContent = information[i];
+    projectInfo.appendChild(title);
+    projectInfo.appendChild(projectContent);
+
+    // div for code and link
+    let linkWrapper = document.createElement("div");
+    linkWrapper.classList.add("link-wrapper");
+    projectInfo.appendChild(linkWrapper);
+
+    // Create div for projectlink and add code link
+    let projectLink = document.createElement("div");
+    projectLink.classList.add("project-link");
+    linkWrapper.appendChild(projectLink);
+    let code = document.createElement("a");
+    code.textContent = "Code";
+    code.title = "Code";
+    code.href = codeLinks[i];
+    projectLink.appendChild(code);
+
+    // Create div for projectlink and add live link
+    projectLink = document.createElement("div");
+    projectLink.classList.add("project-link");
+    linkWrapper.appendChild(projectLink);
+    let live = document.createElement("a");
+    live.textContent = "Live";
+    live.title = "Live";
+    live.href = liveLinks[i];
+    projectLink.appendChild(live);
+
+}
